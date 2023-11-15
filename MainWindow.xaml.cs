af68f006632294a706258f500fc16a19bc9dd072
fishman123456 <den9152@yandex.ru>
2023-11-15 22:32:05 +0700

    1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -14,12 +16,35 @@ namespace shambalaWpfWorkReestr_11_11_2023
         {
             InitializeComponent();
         }
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+        private void test()
+        {
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
 
+        private void dispatcherTimer_Tick(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        // через определенное время записываем в реестр
+        public static void Count(object obj)
+        {
+            int x = (int)obj;
+            proverka.SetExecutingBeforeKey("IS_EXECUTING_BEFORE_KEY",x.ToString());
+        }
         private void GenRand_Click(object sender, RoutedEventArgs e)
         {
-            Timer usertime = new Timer();
-            usertimer.Text = usertime.UserTimer();
-            userreestr.Text = proverka.IsExecutingBefore("IS_EXECUTING_BEFORE_KEY","yes" ).ToString();
+            int num = 0;
+            // устанавливаем метод обратного вызова
+            TimerCallback tm = new TimerCallback(Count);
+            // создаем таймер на 4 секунды
+            Timer timer = new Timer(tm, num, 0, 4000);
+
+            Console.ReadLine();
+            userreestr.Text = proverka.IsExecutingBefore("IS_EXECUTING_BEFORE_KEY","30" ).ToString();
             randomNumbers randomNumbers = new randomNumbers();
             RandomNumber.Text = randomNumbers.getRand(10).ToString();
             if (UserNumber.Text == RandomNumber.Text)
@@ -37,7 +62,7 @@ namespace shambalaWpfWorkReestr_11_11_2023
 
         private void NewGame_Click(object sender, RoutedEventArgs e)
         {
-            proverka.SetExecutingBeforeKey("IS_EXECUTING_BEFORE_KEY", "yes");
+           
         }
 
         private void UserNumber_TextChanged(object sender, TextChangedEventArgs e)
--- Timer.cs
+++ Timer.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace shambalaWpfWorkReestr_11_11_2023
 {
     public class Timer
     {
+        public Timer(TimerCallback tm, int num, int v1, int v2)
+        {
+        }
+
         public string timerTick { get; set; }
         public string UserTimer()
         {
